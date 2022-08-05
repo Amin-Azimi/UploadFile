59ca2238956ca2b35dc9fae5bdd160d42b436f4f
aminazimi <aminazimi@gmail.com>
2022-08-05 17:01:10 +0430

    resize imgae

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -5,46 +5,33 @@ import CreatedFileDto from "./created.file.dto";
 import { v4 as uuidv4 } from 'uuid';
 import { Request } from 'express';
 import { Stream } from "stream";
+import * as fs from 'fs';
+import * as sharp from "sharp";
 
 @Injectable()
 export class FileService{
+    private  resizedImageWidth = 300;
+
     constructor(private readonly configService:ConfigService){}
-
-    async uploadFile(data:Buffer, fileName:string) : Promise<CreatedFileDto>{
-        const s3 = new S3();
-        const uploadedFile = await s3.upload({
-            Bucket: 'lll',
-            Body: data,
-            Key: uuidv4()+"-"+fileName
-        }).promise();
-        return {
-            key :uploadedFile.Key,
-            fileUrl : uploadedFile.Location};
-    }
-
     checkHealth(){
 
         return 'Ok';
     }
-
-    private async  uploadStream(fileName:string) {
-        const s3 = new S3();
-        const pass = new Stream.PassThrough();
-        return {
-            writeStream: pass,
-            promise : await s3.upload({
-                    Bucket: "this.configService.get('AWS_PUBLIC_BUCKET_NAME')",
-                    Body : pass,
-                    Key: uuidv4()+"-"+fileName
-                })
-        }        
-    }
-
-
-
+    //uploading file to S3 by stream
     async uploadFileStream(req : Request,fileName:string) : Promise<CreatedFileDto>{
+
+        const fileNameParts = fileName.split('.');
+        const fileNameExtenstion = fileNameParts[fileNameParts.length-1];
         const { writeStream, promise } = await  this.uploadStream(fileName);
-        req.pipe(writeStream);
+        //if the input file is an image it will resized.
+        if((this.configService.get('VALID_FILE_EXTENSIONS')as string[]).includes(fileNameExtenstion)){        
+
+            const transform = sharp().resize({ width: this.resizedImageWidth, height: this.resizedImageWidth })
+
+            req.pipe(transform).pipe(writeStream);
+        }
+        else
+            req.pipe(writeStream);
         try {
             const uploadedFile = await promise.promise();
             console.log('upload completed successfully',uploadedFile);
@@ -55,6 +42,20 @@ export class FileService{
             catch (error) {
             console.log('upload failed.', error.message);
         }
+    }
+
+   //Uploading  stream to S3 
+    private async  uploadStream(fileName:string) {
+        const s3 = new S3();
+        const pass = new Stream.PassThrough();
+        return {
+            writeStream: pass,
+            promise : await s3.upload({
+                    Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+                    Body : pass,
+                    Key: uuidv4()+"-"+fileName
+                })
+        }        
     }
 
 
